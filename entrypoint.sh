db5407bff4917331316e11535a8f32f092006c28
Max Hoffman <max@dolthub.com>
2021-05-27 10:54:18 -0700

    progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eox pipefail
 
-if [ "$( "$INPUT_SHEETS" | jq 'if type=="array" then "true" else "false" end')" = "false" ]; then
+if [ "$( echo "$INPUT_SHEETS" | jq 'if type=="array" then "true" else "false" end')" = "false" ]; then
     echo "Inputs: sheets must be a valid json array"
     exit 1
 fi
@@ -12,32 +12,35 @@ outputs=()
 
 for row in $(echo "${INPUT_SHEETS}" | jq -r '.[] | @base64'); do
     _jq() {
-      echo ${row} | base64 --decode | jq -r ${1}
+      echo ${row} | base64 -d | jq -r ${1}
     }
 
     id=$(_jq '.id')
     range=$(_jq '.range')
     name=$(_jq '.name')
 
-    if [ -n "${id}" ] || [ -n "${range}" ] || [ -n "${name}" ]; then
+    if [ -z "${id}" ] || [ -z "${range}" ] || [ -z "${name}" ]; then
         row_cnt=$((row_cnt+1))
         continue
     fi
 
     tempfile="${INPUT_TEMPDIR}/${row_cnt}.csv"
 
-    curl \
+    res=$(curl \
+        -G \
         --data-urlencode "range=${name}!${range}" \
         --header "Accept: application/json" \
-        'https://sheets.googleapis.com/v4/spreadsheets/1aaEoK1InHzEYjw6RNpuPvMoR-OKxYKXOjfhb1eE5Y_Y/values:get' \
-        --compressed \
-    | jq -r '.values | .[0], .[1:][] | @csv' \
-    > "${tempfile}"
+        'https://sheets.googleapis.com/v4/spreadsheets/1aaEoK1InHzEYjw6RNpuPvMoR-OKxYKXOjfhb1eE5Y_Y/values' \
+        --compressed)
+        #| jq -r '.values | .[0], .[1:][] | @csv' \
+    #> "${tempfile}"
         #--data-urlencode "key=${INPUT_API_KEY}" \
         #--header "Authorization: Bearer ${INPUT_ACCESS_TOKEN}]" \
+    echo $res
+    #cat $tmpfile
 
     row_cnt=$((row_cnt+1))
-    outputs+=(tempfile)
+    outputs+=("${tempfile}")
 done
 
 echo "::set-output name=results::${outputs}"
